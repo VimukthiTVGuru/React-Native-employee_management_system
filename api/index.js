e629f5311b6192d2f068cd7cfa47b07319105335
Thimira Vimukthi <ictthimira@gmail.com>
2024-02-26 14:24:55 +0530

    design home page, connect mongo DB and  create the schemas

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect("mongodb+srv://vimu:vimu@cluster0.r9p5m1t.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error);
+  });
+
+app.listen(port, () => {
+  console.log("Server is running on port 8000");
+});
