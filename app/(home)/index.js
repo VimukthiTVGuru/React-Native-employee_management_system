8f5b323d6045bc0c6cd107eae2e7964d9c83ad37
Thimira Vimukthi <ictthimira@gmail.com>
2024-07-01 10:08:11 +0530

    react native employee management system

--- app/(home)/index.js
+++ app/(home)/index.js
@@ -2,14 +2,14 @@ import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import { Feather, Entypo, Ionicons, Octicons } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
+import Summary from "./summary";
 
 const index = () => {
-    const router = useRouter();
+  const router = useRouter();
   return (
     <ScrollView>
-      <LinearGradient colors={["#7F7FD5", "#E9E4F0"]} style={{ flex: 1 }}>
+      <LinearGradient colors={["#64B5F6", "#7986CB"]} style={{ flex: 1 }}>
         <View style={{ padding: 12 }}>
           <View
             style={{
@@ -18,11 +18,11 @@ const index = () => {
               justifyContent: "space-between",
             }}
           >
-            <Feather name="bar-chart" size={24} color="black" />
-            <Text style={{ fontSize: 16, fontWeight: "600" }}>
-              Employee Managament System
+            <Feather name="bar-chart" size={24} color="#FFFFFF" />
+            <Text style={{ fontSize: 18, fontWeight: "700", color: "#FFFFFF" }}>
+              Employee Management System
             </Text>
-            <Entypo name="lock" size={24} color="black" />
+            <Entypo name="lock" size={24} color="#FFFFFF" />
           </View>
 
           <View
@@ -34,11 +34,11 @@ const index = () => {
             }}
           >
             <Pressable
-           onPress={() => router.push("/(home)/employees")}
+              onPress={() => router.push("/(home)/employees")}
               style={{
-                backgroundColor: "#D3CCE3",
+                backgroundColor: "#27AE60",
                 padding: 12,
-                borderRadius: 6,
+                borderRadius: 15,
                 alignItems: "center",
                 justifyContent: "center",
                 flex: 1,
@@ -49,23 +49,23 @@ const index = () => {
                   width: 50,
                   height: 50,
                   borderRadius: 25,
-                  backgroundColor: "white",
+                  backgroundColor: "#64B5F6",
                   alignItems: "center",
                   justifyContent: "center",
                 }}
               >
-                <Ionicons name="people-sharp" size={24} color="black" />
+                <Ionicons name="people-sharp" size={24} color="#FFFFFF" />
               </View>
-              <Text style={{ marginTop: 7, fontWeight: "600" }}>
+              <Text style={{ marginTop: 7, fontWeight: "600", color: "#FFFFFF" }}>
                 Employee List
               </Text>
             </Pressable>
             <Pressable
               onPress={() => router.push("/(home)/markattendance")}
               style={{
-                backgroundColor: "#D3CCE3",
+                backgroundColor: "#E74C3C",
                 padding: 12,
-                borderRadius: 6,
+                borderRadius: 15,
                 alignItems: "center",
                 justifyContent: "center",
                 flex: 1,
@@ -76,14 +76,14 @@ const index = () => {
                   width: 50,
                   height: 50,
                   borderRadius: 25,
-                  backgroundColor: "white",
+                  backgroundColor: "#64B5F6",
                   alignItems: "center",
                   justifyContent: "center",
                 }}
               >
-                <Ionicons name="people-sharp" size={24} color="black" />
+                <Ionicons name="people-sharp" size={24} color="#FFFFFF" />
               </View>
-              <Text style={{ marginTop: 7, fontWeight: "600" }}>
+              <Text style={{ marginTop: 7, fontWeight: "600", color: "#FFFFFF" }}>
                 Mark Attendance
               </Text>
             </Pressable>
@@ -91,15 +91,15 @@ const index = () => {
           <View
             style={{
               marginTop: 20,
-              backgroundColor: "white",
+              backgroundColor: "#FFFFFF",
               paddingHorizontal: 10,
               paddingVertical: 10,
-              borderRadius: 7,
             }}
           >
-            <Pressable
+            <View
+              onPress={() => router.push("/(home)/summary")}
               style={{
-                backgroundColor: "#BE93C5",
+                backgroundColor: "#3498DB",
                 borderRadius: 6,
                 padding: 10,
                 flexDirection: "row",
@@ -113,300 +113,28 @@ const index = () => {
                   width: 45,
                   height: 45,
                   borderRadius: 7,
-                  backgroundColor: "white",
+                  backgroundColor: "#E74C3C",
                   alignItems: "center",
                   justifyContent: "center",
                 }}
               >
-                <Ionicons name="newspaper-outline" size={24} color="black" />
+                <Octicons name="repo-pull" size={24} color="#ECF0F1" />
               </View>
               <Text
                 style={{
                   marginLeft: 10,
                   fontSize: 16,
-                  fontWeight: "600",
+                  fontWeight: "700",
                   flex: 1,
-                }}
-              >
-                Attendance Report
-              </Text>
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Entypo name="chevron-right" size={24} color="black" />
-              </View>
-            </Pressable>
-            <Pressable
-                onPress={() => router.push("/(home)/summary")}
-              style={{
-                backgroundColor: "#BE93C5",
-                borderRadius: 6,
-                padding: 10,
-                flexDirection: "row",
-                alignItems: "center",
-                marginVertical: 10,
-              }}
-            >
-              <View
-                style={{
-                  padding: 7,
-                  width: 45,
-                  height: 45,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Octicons name="repo-pull" size={24} color="black" />
-              </View>
-              <Text
-                style={{
-                  marginLeft: 10,
-                  fontSize: 16,
-                  fontWeight: "600",
-                  flex: 1,
+                  color: "#ECF0F1",
                 }}
               >
                 Summary Report
               </Text>
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Entypo name="chevron-right" size={24} color="black" />
-              </View>
-            </Pressable>
-            <Pressable
-              style={{
-                backgroundColor: "#BE93C5",
-                borderRadius: 6,
-                padding: 10,
-                flexDirection: "row",
-                alignItems: "center",
-                marginVertical: 10,
-              }}
-            >
-              <View
-                style={{
-                  padding: 7,
-                  width: 45,
-                  height: 45,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Octicons name="report" size={24} color="black" />
-              </View>
-              <Text
-                style={{
-                  marginLeft: 10,
-                  fontSize: 16,
-                  fontWeight: "600",
-                  flex: 1,
-                }}
-              >
-                All Generate Reports
-              </Text>
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Entypo name="chevron-right" size={24} color="black" />
-              </View>
-            </Pressable>
-            <Pressable
-              style={{
-                backgroundColor: "#BE93C5",
-                borderRadius: 6,
-                padding: 10,
-                flexDirection: "row",
-                alignItems: "center",
-                marginVertical: 10,
-              }}
-            >
-              <View
-                style={{
-                  padding: 7,
-                  width: 45,
-                  height: 45,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Ionicons name="people" size={24} color="black" />
-              </View>
-              <Text
-                style={{
-                  marginLeft: 10,
-                  fontSize: 16,
-                  fontWeight: "600",
-                  flex: 1,
-                }}
-              >
-                Overtime Employees
-              </Text>
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Entypo name="chevron-right" size={24} color="black" />
-              </View>
-            </Pressable>
+            </View>
           </View>
-          <View
-            style={{
-              marginTop: 20,
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 12,
-            }}
-          >
-            <View
-              style={{
-                backgroundColor: "#f79d00",
-                borderRadius: 6,
-                padding: 12,
-                alignItems: "center",
 
-                justifyContent: "center",
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <MaterialCommunityIcons
-                  name="guy-fawkes-mask"
-                  size={24}
-                  color="black"
-                />
-              </View>
-              <Text style={{ marginTop: 7 }}>Attendance Criteria</Text>
-            </View>
-            <View
-              style={{
-                backgroundColor: "#ABCABA",
-                borderRadius: 6,
-                padding: 12,
-                alignItems: "center",
-                justifyContent: "center",
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Feather name="bar-chart" size={24} color="black" />
-              </View>
-              <Text style={{ marginTop: 7 }}>Increased Workflow</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              marginTop: 20,
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 12,
-            }}
-          >
-            <View
-              style={{
-                backgroundColor: "#D3CCE3",
-                borderRadius: 6,
-                padding: 12,
-                alignItems: "center",
-
-                justifyContent: "center",
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <MaterialCommunityIcons
-                  name="guy-fawkes-mask"
-                  size={24}
-                  color="black"
-                />
-              </View>
-              <Text style={{ marginTop: 7 }}>Cost Savings</Text>
-            </View>
-            <View
-              style={{
-                backgroundColor: "#bdc3c7",
-                borderRadius: 6,
-                padding: 12,
-                alignItems: "center",
-                justifyContent: "center",
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Feather name="bar-chart" size={24} color="black" />
-              </View>
-              <Text style={{ marginTop: 7 }}>Employee Performance</Text>
-            </View>
-          </View>
+          <Summary />
         </View>
       </LinearGradient>
     </ScrollView>
